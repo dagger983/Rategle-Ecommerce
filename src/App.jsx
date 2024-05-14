e5aaeb6fbeafa1afde215e8d8289c37e050c6f18
dagger983 <kmvignesh1406@gmail.com>
2024-05-15 02:16:38 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
       ) : (
         <>
           <Navbar />
-          <Test/>
+         
           <Category />
           <CarouselFlipkart />
           <BestDealsMobiles />
