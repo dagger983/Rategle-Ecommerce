8c32eb742627cf08a2bd2e0506c6f00908bc3c18
dagger983 <kmvignesh1406@gmail.com>
2024-05-15 02:23:22 +0530

    first commit

--- src/components/rotateerror/rotateerror.jsx
+++ src/components/rotateerror/rotateerror.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./rotateerror.css"
 function RotateError() {
   return (
-    <div><img src="./src/assets/rotaterr.png"  className='page-err' alt="" /></div>
+    <div><img src="https://res.cloudinary.com/dzmwbkdtr/image/upload/v1715718549/rotaterr_o5ncgm.png"  className='page-err' alt="" /></div>
   )
 }
 
