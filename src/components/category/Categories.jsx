acc7bcc546a9bc64c483e6001aa5c7161072b001
dagger983 <kmvignesh1406@gmail.com>
2024-05-15 01:34:54 +0530

    first commit

--- src/components/category/Categories.jsx
+++ src/components/category/Categories.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import "./Categories.css"
+import React from 'react';
+import groceryImage from './assets/grocery.png';
+import mobilesImage from './assets/mobiles.png';
+import fashionImage from './assets/fashion.png';
+import laptopImage from './assets/laptop.png';
+import "./Categories.css";
+
 function Category() {
   return (
     <div className='category-card'>
       <div className='category-card-main'>
-      <div className='cat-card' >
-          <img src="./src/assets/grocery.png" alt="" className='cate-card-img'/>
+        <div className='cat-card' >
+          <img src={groceryImage} alt="Grocery" className='cate-card-img'/>
           <p>Grocery</p>
-      </div>
-      <div  className='cat-card' >
-          <img src="./src/assets/mobiles.png" alt="" className='cate-card-img'/>
+        </div>
+        <div className='cat-card' >
+          <img src={mobilesImage} alt="Mobiles" className='cate-card-img'/>
           <p>Mobiles</p>
-      </div>
-      <div  className='cat-card' >
-          <img src="./src/assets/fashion.png" alt="" className='cate-card-img'/>
+        </div>
+        <div className='cat-card' >
+          <img src={fashionImage} alt="Fashion" className='cate-card-img'/>
           <p>Fashion</p>
-      </div>
-      <div  className='cat-card' >
-          <img src="./src/assets/laptop.png" alt="" className='cate-card-img'/>
+        </div>
+        <div className='cat-card' >
+          <img src={laptopImage} alt="Electronics" className='cate-card-img'/>
           <p>Electronics</p>
+        </div>
       </div>
-  
     </div>
-
-    </div>
-    
-  )
+  );
 }
 
 export default Category;
