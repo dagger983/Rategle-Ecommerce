023392310fb37f68c6ec2de4cf787f74488bbd14
dagger983 <kmvignesh1406@gmail.com>
2024-05-25 17:23:16 +0530

    first commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './footer.css'
+import { Link } from 'react-router-dom';
 function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer>
-      <p>© {currentYear} &#160; Rategle Technologies <br /> All rights reserved&#160; |&#160; Designed and Developed by Rategle Technologies&#160; | <br /> <a href="tel:+916384311620" className='phone-num'>Phone &#160; :&#160; +91&#160; 6384311620 </a> &#160;|&#160; Privacy Policy&#160; |&#160; Terms of Service &#160;</p>
+      <p><Link to="/admin" style={{ textDecoration: 'none', color: 'inherit',cursor:"pointer" }}>© {currentYear}</Link> &#160; Rategle Technologies <br /> All rights reserved&#160; |&#160; Designed and Developed by Rategle Technologies&#160; | <br /> <a href="tel:+916384311620" className='phone-num'>Phone &#160; :&#160; +91&#160; 6384311620 </a> &#160;|&#160; Privacy Policy&#160; |&#160; Terms of Service &#160;</p>
     </footer> 
   );
 }
